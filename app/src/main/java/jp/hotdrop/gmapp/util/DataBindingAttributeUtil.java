71000fa35af90ac70580e500148705a0179de2ac
kenji <gunsecondstage0106@gmail.com>
2016-08-14 12:45:25 +0900

    change amount imageIcon and transform circle shape with amount image view.

--- app/src/main/java/jp/hotdrop/gmapp/util/DataBindingAttributeUtil.java
+++ app/src/main/java/jp/hotdrop/gmapp/util/DataBindingAttributeUtil.java
@@ -3,29 +3,62 @@ package jp.hotdrop.gmapp.util;
 import android.databinding.BindingAdapter;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 import jp.hotdrop.gmapp.R;
 import jp.hotdrop.gmapp.model.Goods;
 
 public class DataBindingAttributeUtil {
 
-    @BindingAdapter("amountImageUrl")
-    public static void setAmountImage(ImageView iv, int amount) {
+    @BindingAdapter({"amountImage", "amountImageSize"})
+    public static void setAmountImage(ImageView imageView, int amount, float sizeInDimen) {
 
         switch(amount) {
             case Goods.AMOUNT_FULL:
-                iv.setImageResource(R.drawable.amount_full);
+                setImageWithCircle(imageView, R.drawable.amount_full, sizeInDimen);
                 break;
             case Goods.AMOUNT_MANY:
-                iv.setImageResource(R.drawable.amount_many);
+                setImageWithCircle(imageView, R.drawable.amount_many, sizeInDimen);
                 break;
             case Goods.AMOUNT_HALF:
-                iv.setImageResource(R.drawable.amount_half);
+                setImageWithCircle(imageView, R.drawable.amount_half, sizeInDimen);
                 break;
             case Goods.AMOUNT_A_LITTLE:
-                iv.setImageResource(R.drawable.amount_little);
+                setImageWithCircle(imageView, R.drawable.amount_little, sizeInDimen);
                 break;
             default:
-                iv.setImageResource(R.drawable.amount_empty);
+                setImageWithCircle(imageView, R.drawable.amount_empty, sizeInDimen);
+                break;
+        }
+    }
+
+    private static void setImageWithCircle(ImageView imageView, int amountResId, float sizeInDimen) {
+        final int size = Math.round(sizeInDimen);
+        Picasso.with(imageView.getContext()).load(amountResId)
+                .resize(size, size)
+                .centerInside()
+                .transform(new CircleTransform())
+                .into(imageView);
+    }
+
+    @BindingAdapter("amountImageCanChange")
+    public static void changeAmountImage(ImageView imageView, int amount) {
+
+        switch(amount) {
+            case Goods.AMOUNT_FULL:
+                imageView.setImageResource(R.drawable.amount_full);
+                break;
+            case Goods.AMOUNT_MANY:
+                imageView.setImageResource(R.drawable.amount_many);
+                break;
+            case Goods.AMOUNT_HALF:
+                imageView.setImageResource(R.drawable.amount_half);
+                break;
+            case Goods.AMOUNT_A_LITTLE:
+                imageView.setImageResource(R.drawable.amount_little);
+                break;
+            default:
+                imageView.setImageResource(R.drawable.amount_empty);
                 break;
         }
     }
